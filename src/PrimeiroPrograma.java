116fdb40c4c1efea5bda4969a5a7549742cc7fa3
Leandro Ferreira de Andrade <leandrofa1980@gmail.com>
2021-07-27 21:17:11 -0300

    First commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!");
+		System.out.println(a + b);*/
+	}
+
+}
